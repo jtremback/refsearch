8802c0c952a81410ed20ac2f37bd0e3ab693c18e
Jehan Tremback <jehan.tremback@gmail.com>
2013-03-03 22:17:32 -0800

    --amend

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -1,7 +1,8 @@
 var url = "http://backbonejs.org/";
-
+var array = []
 
 $("[id]").each(function() {
+  var obj = {};
   var id = ($(this).attr("id"));
   var code = $(this).find("code").text();
   console.log(id);
@@ -10,6 +11,23 @@ $("[id]").each(function() {
   obj.language = "js";
   obj.lib = "backbone";
   obj.func = code;
-
-
+  array.push(obj)
 });
+console.log(JSON.stringify(array));
+
+// var prizzint = function(obj, maxDepth, prefix) {
+//   var key, result;
+//   result = "";
+//   for (key in obj) {
+//     if (typeof obj[key] === "object") {
+//       if (maxDepth !== undefined && maxDepth <= 1) {
+//         result += key + " : object [max depth reached]\n";
+//       } else {
+//         result += this.prizzint(obj[key], (maxDepth ? maxDepth - 1 : maxDepth), key + ".");
+//       }
+//     } else {
+//       result += key + " : " + obj[key] + "\n";
+//     }
+//   }
+//   return result;
+// }
