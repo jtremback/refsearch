5dc102b79d0118ac2800d9b6b3817d4a4326b0cc
Jehan Tremback <jehan.tremback@gmail.com>
2013-03-04 19:55:45 -0800

    basic working v. needs design

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -38,17 +38,9 @@
 
 // console.log(JSON.stringify(array));
 
-var people = [{person: { id: 1, name: 'Jiren', age:26, country: 'India', country_id: 1,
-        states : [{ state : 'MH', state_id : 3 }, {state : 'HN', state_id : 4}] } },
-{person: {id: 2, name: 'Joe', age: 25, country: 'USA', country_id: 2,
-        states : [{ state : 'MH', state_id : 3 }, {state : 'HN', state_id : 4}] } }
-        ];
-var view = function(person){
-    detail = '<p>'+person.name+' '+person.age+' ('+person.country+')</p>';
-    return this.link('/demo/' + person.id, { 'title': person.name }, detail);
+
+var options = {
+    valueNames: [ 'func' ]
 };
 
-var settings = {
-  search: {input: "#searchbox"}
-};
-var fJS = FilterJS(people, '#people_list', view, settings);
+var hackerList = new List('func-list', options);
