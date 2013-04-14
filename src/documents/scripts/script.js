ac12e8d773c2cff0ff3889b9365a949d5004b55b
Jehan Tremback <jehan.tremback@gmail.com>
2013-04-14 12:04:37 -0700

    heavy refactor of script.js

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -1,83 +1,88 @@
+var libLoad = function(toget) {
+  
+  //Add sequential number to ref units and add them to lunr idx
+  var bagNtag = function(db) {
+    for (i = 0; i < db.length; i++) {
+      var ref_unit = db[i];
+      ref_unit.id = i;
+      idx.add(db[i])
+    }
+  }
 
-//Add sequential number to ref units and add them to lunr idx
-var idx = lunr(function () {
-	this.field('func', { boost: 10 })
-})
+  var deferredArr = $.map(toget, function(el, i) {
+    return $.getJSON(el, function(json) {
+      db.push.apply(db, json);
+    });
 
-function bagNtag(db) {
-	for (i = 0; i < db.length; i++) {
-		var ref_unit = db[i];
-		ref_unit.id = i;
-		idx.add(db[i])
-	}
-	console.log("bagntag: " + db);
+  });
+
+  $.when.apply(this, deferredArr).then(function() {
+      bagNtag(db);
+  });
 }
 
-
-var toget = ["data/backbone.json", "data/underscore.json"];
-var db = [];
-
-var deferredArr = $.map(toget, function(el, i) {
-	return $.getJSON(el, function(json) {
-		db.push.apply(db, json);
-	});
-
-});
-
-$.when.apply(this, deferredArr).then(function() {
-		console.log("All Done Loading!" + db);
-		bagNtag(db);
-});
 
 
 //Live Search
 //Reacts to keyup in the input and waits for a certain amount of time before
 //calling searc func
 var typewatch = function(callback, ms){
-		console.log("typewatch");
-		var timer = 0;
-		return function(callback, ms){
-				clearTimeout (timer);
-				timer = setTimeout(callback, ms);
-		}
+    var timer = 0;
+    return function(callback, ms){
+        clearTimeout (timer);
+        timer = setTimeout(callback, ms);
+    }
 }();
 
 
 
+var searchRenderer = function(inputfield, resultslist){
 
-//Get results for query
-//Get array of corresponding documents
-//Render template with document array
-var searchRender = function(results) {
-	var results_length = results.length;
-	var results_arr = [];
-	i = 0;
+  //Get results for query
+  //Get array of corresponding documents
+  //Render template with document array
+  var render = function(results) {
+    var results_arr = [];
+    i = 0;
 
+    while (i < results.length) {
+      var ref = results[i].ref;
+      results_arr.push(db[ref])
+      i++
+    }
+    var templ_fn = doT.template(document.getElementById('result_tmpl').text)
+    document.getElementById('results-list').innerHTML = templ_fn(results_arr);
+  }
 
-	while (i < results_length) {
-		var rel = results[i].score
-		var ref = results[i].ref;
-		results_arr.push(db[ref])
-		i++
-	}
-
-	var result_fn = doT.template(document.getElementById('result_tmpl').text)
-	document.getElementById('results-list').innerHTML = result_fn(results_arr);
+  //On keyup in inputfield, call typewatch with callback
+  //If the input is too short, kill the results.
+  //If it is of a sufficient length, render the list with the lunr results.
+  inputfield.on('keyup', function () {
+    var that = this;
+    typewatch(function(){
+      var value = that.value;
+      if (value.length > 1) {
+        render(idx.search(value))
+      } else {
+        resultslist.html('');
+      }
+    }, 200)
+  })
 }
 
+
+
+
+var idx = lunr(function () {
+  this.field('func', { boost: 10 })
+})
+
+var db = [];
+
 $(document).ready(function () {
-	console.log("ready");
-	document.getElementById('search').onkeyup = function () {
-		typewatch(function(){
-			var value = document.getElementById('search').value;
-			if (value.length > 1) {
-				$('#searchtainer').addClass('dirty');
-				searchRender(idx.search(document.getElementById('search').value))
-			} else {
-				document.getElementById('results-list').innerHTML = '';
-			}
-		}, 200)
-		}
+  libLoad(["data/backbone.json", "data/underscore.json"]);
+  searchRenderer($('#search'), $('#results-list'));
+
 });
 
 
